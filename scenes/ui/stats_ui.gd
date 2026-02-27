5f3d6578d2a8d9ce17a52b93f000bc587de4555c
rkobus <radek109@o2.pl>
2026-02-27 22:11:31 +0100

    Eighteenth commit - Creating the Player Scene

--- scenes/ui/stats_ui.gd
+++ scenes/ui/stats_ui.gd
@@ -1,14 +1,14 @@
 class_name StatsUI
-extends HBoxContainer
+extends VBoxContainer
 
-@onready var block: HBoxContainer = $Block
-@onready var block_label: Label = %BlockLabel
-@onready var health: HBoxContainer = $Health
-@onready var health_label: Label = %HealthLabel
+@onready var health: Panel = $TopRow/HBoxContainer/HealthPanel
+@onready var health_label: Label = $TopRow/HBoxContainer/HealthPanel/HealthLabel
+@onready var block: Panel = $TopRow/HBoxContainer/BlockPanel
+@onready var block_label: Label = $TopRow/HBoxContainer/BlockPanel/BlockLabel
 
 func update_stats(stats: Stats) -> void:
 	block_label.text = str(stats.block)
 	health_label.text = str(stats.health)
 
 	block.visible = stats.block > 0
-	health. visible = stats.health > 0
+	health.visible = stats.health > 0
