acc98caf3868efcd333a9dde08aa8b0124ec0703
rkobus <radek109@o2.pl>
2026-03-08 14:14:49 +0100

    Twenty-sixth commit - Create tooltip

--- global/events.gd
+++ global/events.gd
@@ -6,4 +6,7 @@ signal card_drag_ended(card_ui: CardUI)
 signal card_aim_started(card_ui: CardUI)
 signal card_aim_ended(card_ui: CardUI)
 signal card_played(card: Card)
+signal card_tooltip_requested(card: Card)
+signal tooltip_hide_requested
+
 signal player_hit
--- scenes/card_ui/card_states/card_base_state.gd
+++ scenes/card_ui/card_states/card_base_state.gd
@@ -9,6 +9,7 @@ func enter() -> void:
 	
 	card_ui.reparent_requested.emit(card_ui)
 	card_ui.pivot_offset = Vector2.ZERO
+	Events.tooltip_hide_requested.emit()
 
 func on_gui_input(event: InputEvent) -> void:
 	if not card_ui.playable or card_ui.disabled:
@@ -17,3 +18,15 @@ func on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
 		card_ui.pivot_offset = card_ui.get_global_mouse_position() - card_ui.global_position
 		transition_requested.emit(self, CardState.State.CLICKED)
+
+func on_mouse_entered() -> void:
+	if not card_ui.playable or card_ui.disabled:
+		return
+	
+	Events.card_tooltip_requested.emit(card_ui.card.icon, card_ui.card.tooltip_text)
+
+func on_mouse_exited() -> void:
+	if not card_ui.playable or card_ui.disabled:
+		return
+	
+	Events.tooltip_hide_requested.emit()
