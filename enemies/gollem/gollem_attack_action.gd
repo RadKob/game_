049b719c085c60a2a5108fabee3fb6c8e33ba118
rkobus <radek109@o2.pl>
2026-04-12 20:18:57 +0200

    better ui - part 5

--- enemies/gollem/gollem_attack_action.gd
+++ enemies/gollem/gollem_attack_action.gd
@@ -8,7 +8,8 @@ func perform_action() -> void:
 	
 	var tween := create_tween().set_trans(Tween.TRANS_QUINT)
 	var start := enemy.global_position
-	var end := target.global_position + Vector2.RIGHT * 32
+	var end := start + Vector2.LEFT * 10
+	#var end := target.global_position + Vector2.RIGHT * 32
 	var damage_effect := DamageEffect.new()
 	var target_array: Array[Node] = [target]
 	damage_effect.amount = damage
--- scenes/ui/hand.gd
+++ scenes/ui/hand.gd
@@ -23,6 +23,6 @@ func disable_hand() -> void:
 func _on_card_ui_reparent_requested(child: CardUI) -> void:
 	child.disabled = true
 	child.reparent(self)
-	var new_index := clampi(child.oryginal_index, 0, get_child_count())
+	var new_index := clampi(child.original_index, 0, get_child_count())
 	move_child.call_deferred(child, new_index)
 	child.set_deferred("disabled", false)
