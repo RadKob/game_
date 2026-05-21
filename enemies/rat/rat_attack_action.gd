d9456d881a1a179d449feace80a18c8bffe586a6
rkobus <radek109@o2.pl>
2026-05-21 19:20:04 +0200

    better ui - part 13

---
+++ enemies/rat/rat_attack_action.gd
@@ -0,0 +1,25 @@
+extends EnemyAction
+
+@export var damage := 11
+
+func perform_action() -> void:
+	if not enemy or not target:
+		return
+	
+	var tween := create_tween().set_trans(Tween.TRANS_QUINT)
+	var start := enemy.sprite_2d.global_position
+	var end := start + Vector2.LEFT * 10
+	var damage_effect := DamageEffect.new()
+	var target_array: Array[Node] = [target]
+	damage_effect.amount = damage
+	damage_effect.sound = sound
+	
+	tween.tween_property(enemy.sprite_2d, "global_position", end, 0.4)
+	tween.tween_callback(damage_effect.execute.bind(target_array))
+	tween.tween_interval(0.25)
+	tween.tween_property(enemy.sprite_2d, "global_position", start, 0.4)
+	
+	tween.finished.connect(
+		func():
+			Events.enemy_action_completed.emit(enemy)
+	)
