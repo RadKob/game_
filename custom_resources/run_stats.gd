9cf704f53abf6519a36a02f6359bbb401b246ecc
rkobus <radek109@o2.pl>
2026-08-06 18:41:29 +0200

    Create BattleRewards scene

--- custom_resources/run_stats.gd
+++ custom_resources/run_stats.gd
@@ -4,9 +4,22 @@ extends Resource
 signal gold_changed
 
 const STARTING_GOLD := 70
+const BASE_CARD_REWARDS := 3
+const BASE_COMMON_WEIGHT := 6.0
+const BASE_RARE_WEIGHT := 3.7
+const BASE_EPIC_WEIGHT := 0.3
 
 @export var gold := STARTING_GOLD : set = set_gold
+@export var card_rewards := BASE_CARD_REWARDS
+@export_range(0.0, 10.0) var common_weight := BASE_COMMON_WEIGHT
+@export_range(0.0, 10.0) var rare_weight := BASE_RARE_WEIGHT
+@export_range(0.0, 10.0) var epic_weight := BASE_EPIC_WEIGHT
 
 func set_gold(new_ampount: int) -> void:
 	gold = new_ampount
 	gold_changed.emit()
+
+func reset_weights() -> void:
+	common_weight = BASE_COMMON_WEIGHT
+	rare_weight = BASE_RARE_WEIGHT
+	epic_weight = BASE_EPIC_WEIGHT
