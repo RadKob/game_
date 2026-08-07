9a5f78724280c68a4e9158b831e98e052cb6a375
rkobus <radek109@o2.pl>
2026-08-07 21:16:39 +0200

    Create BattleRewards - Gold

--- scenes/battle_reward/battle_reward.gd
+++ scenes/battle_reward/battle_reward.gd
@@ -1,4 +1,37 @@
+class_name BattleReward
 extends Control
 
-func _on_button_pressed() -> void:
+enum Type {GOLD, NEW_CARD, RELIC}
+
+const REWARD_BUTTON = preload("res://scenes/ui/reward_button.tscn")
+const GOLD_ICON := preload("res://art/money.png")
+const GOLD_TEXT := "%s gold"
+const CARD_ICON := preload("res://art/card_reward_icon.png")
+const CARD_TEXT := "Add New Card"
+
+@export var run_stats: RunStats
+
+@onready var rewards: VBoxContainer = %Rewards
+
+func _ready() -> void:
+	for node: Node in rewards.get_children():
+		node.queue_free()
+	
+	run_stats = RunStats.new()
+	run_stats.gold_changed.connect(func(): print("gold: %s" % run_stats.gold))
+
+func add_gold_reward(amount: int) -> void:
+	var gold_reward := REWARD_BUTTON.instantiate() as RewardButton
+	gold_reward.reward_icon = GOLD_ICON
+	gold_reward.reward_text = GOLD_TEXT % amount
+	gold_reward.pressed.connect(_on_gold_reward_taken.bind(amount))
+	rewards.add_child.call_deferred(gold_reward)
+
+func _on_gold_reward_taken(amount: int) -> void:
+	if not run_stats:
+		return
+	
+	run_stats.gold += amount
+
+func _on_back_button_pressed() -> void:
 	Events.battle_reward_exited.emit()
